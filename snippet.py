347dbff27ac51ead35c02da09473e1f9179e90e5
Andrés Tomás Abastante <61855794+AndresAbastante@users.noreply.github.com>
2022-04-14 14:21:13 -0300

    Minor variable & comment changes

--- snippet.py
+++ snippet.py
@@ -7,7 +7,7 @@ import sys
 
 url = 'insert_your_target_url'
 
-#turning the 'request.txt' file (in the script folder) into the string variable 'text_file'
+#Turning the 'request.txt' file (in the script folder) into the string variable 'text_file'
 with open(os.path.join(sys.path[0], "request.txt"),"r") as text_file:
     request=text_file.read()
     text_file.close()
@@ -15,28 +15,32 @@ with open(os.path.join(sys.path[0], "request.txt"),"r") as text_file:
 print("##############REQUEST############## \n###################################")
 print(request)
 
-#sending the method over the 'response' variable
+#Sending the method over the 'response' variable
 response = requests.post(url, data = request)
 
 print("##############RESPONSE############## \n###################################")
 response_text=response.text
 print(response_text)
 
-#extracting relevant data from the response string
-print("#############EXTRACTING############# \n####################################")
+#Setting fist and last string targets. They need to be unique, kinda sucks tbh.
+first_string="insert the string parameter before your target"
+last_string="insert the string parameter after your target"
 
-#print() would extract everything 
-#extract=response_text.partition('text_parameter')[2] snippets everything after the text_parameter 
-#extract=re.split("text_parameter+", response_text) snippets every instance of text_parameter found 
-
-#find $ print the first character's position
-start = response_text.find("first_string_to_find") + len("first_string_to_find") 
+#Find & print the first character's position.
+start = response_text.find("first_string") + len("first_string") 
 print(start)
 
-#find & print the last character's position
-end = response_text.find("last_string_to_find")
+#Find & print the last character's position.
+end = response_text.find("last_string")
 print(end)
 
-#cut the string to desirable size
+#Extracting relevant data from the response string.
+print("#############EXTRACTING############# \n####################################")
+
+#Print() would extract everything.
+#Extract=response_text.partition('text_parameter')[2] snippets everything after the text_parameter 
+#Extract=re.split("text_parameter+", response_text) snippets every instance of text_parameter found 
+
+#Cut the string to desirable size.
 extract = response_text[start:end]
 print(extract)
